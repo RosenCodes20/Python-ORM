11bc09b44e2953ed7c340c0ddc4cb40f0df912dc
Rosen Ivanov <143969365+RosenCodes20@users.noreply.github.com>
2024-07-22 18:07:01 +0300

    Update models.py

--- sqlalchemy_ex/models.py
+++ sqlalchemy_ex/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 
-DATABASE = "postgresql+psycopg://postgres:rosen12345@localhost:5432/alchemy_ex"
+DATABASE = ""
 
 engine = create_engine(DATABASE)
 
