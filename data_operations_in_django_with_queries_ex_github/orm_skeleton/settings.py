8974dd1b5589d444c816820b26553a02b01234cb
Rosen Ivanov <143969365+RosenCodes20@users.noreply.github.com>
2024-09-14 01:55:41 +0300

    Update settings.py

--- data_operations_in_django_with_queries_ex_github/orm_skeleton/settings.py
+++ data_operations_in_django_with_queries_ex_github/orm_skeleton/settings.py
@@ -79,11 +79,11 @@ WSGI_APPLICATION = 'orm_skeleton.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "data_operations_in_django_with_queries_ex",
-        "USER": "postgres",
-        "PASSWORD": "rosen12345",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "NAME": "",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
     }
 }
 
