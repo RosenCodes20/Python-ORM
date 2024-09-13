b297b68b9180246e7261468873649c308e1fd8fb
Rosen Ivanov <143969365+RosenCodes20@users.noreply.github.com>
2024-09-14 01:54:02 +0300

    Update settings.py

--- advanced_model_techniques_github/orm_skeleton/settings.py
+++ advanced_model_techniques_github/orm_skeleton/settings.py
@@ -79,11 +79,11 @@ WSGI_APPLICATION = 'orm_skeleton.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "advanced_model_techniques",
-        "USER": "postgres",
-        "PASSWORD": "rosen12345",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "NAME": "",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
     }
 }
 
